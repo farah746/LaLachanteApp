e67558960b96ec4eccb5c3438a00791bd34865c6
harenait <harenait.lalachante@gmail.com>
2023-01-16 18:04:26 +0100

    Third commit

--- app/Http/Controllers/AdministrationlistesController.php
+++ app/Http/Controllers/AdministrationlistesController.php
@@ -8,6 +8,6 @@ class AdministrationlistesController extends Controller
 {
     public function index()
     {
-        return view('administrationlistes');
+        return view('administrationlistes'); 
     }
 }
